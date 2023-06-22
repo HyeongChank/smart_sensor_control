df0078fc7870980e445121a483f56211a34938a7
HC <122770625+HyeongChank@users.noreply.github.com>
2023-06-22 17:56:12 +0900

    update

--- simulation_make_product/simulator.py
+++ simulation_make_product/simulator.py
@@ -1,21 +1,64 @@
 import simpy
+import random
+# from picamera import PiCamera
+# import RPi.GPIO as GPIO
+# from time import sleep
+# import time   
 
-def machine_A(env, name, conveyor):
-    while True:
-        yield env.timeout(2)  # 머신 A에서 제품을 제작하는 데 2시간 소요
-        yield conveyor.put('제품 %s' % name)
-        print('시간 %d: 머신 A가 제품 %s를 완성하고 컨베이어에 넣음' % (env.now, name))
 
-def machine_B(env, conveyor):
-    while True:
-        item = yield conveyor.get()
-        yield env.timeout(3)  # 머신 B에서 제품을 완성하는 데 3시간 소요
-        print('시간 %d: 머신 B가 %s를 완성' % (env.now, item))
+# 생산품 3개
+product_a_count =0
+product_b_count =0
+product_c_count =0
+
+class Product:
+    def __init__(self, env, name, conveyor):
+        self.env = env
+        self.name = name
+        self.conveyor = conveyor
+        if name =='head1':
+            self.action = env.process(self.machine_A1())
+        elif name =='body2':
+            self.action = env.process(self.machine_A2())
+    # def product_generate(self):
+
+
+
+    def machine_A1(self):
+        global product_a_count
+        while True:
+            yield env.timeout(2)  # 머신 A에서 제품을 제작하는 데 2시간 소요
+            product_a_count +=1
+            yield self.conveyor.put('제품 %s' % self.name + str(product_a_count))
+
+            print('시간 %d: 머신 A1가 제품 %s를 완성하고 컨베이어에 넣음' % (env.now, self.name + str(product_a_count)))
+            self.env.process(self.machine_B())
+
+    def machine_A2(self):
+        global product_b_count
+        while True:
+            yield env.timeout(4)  # 머신 A에서 제품을 제작하는 데 2시간 소요
+            product_b_count +=1
+            yield self.conveyor.put('제품 %s' % self.name + str(product_b_count))
+
+            print('시간 %d: 머신 A1가 제품 %s를 완성하고 컨베이어에 넣음' % (env.now, self.name + str(product_b_count)))
+            self.env.process(self.machine_B())
+ 
+    def machine_B(self):
+        while True:
+            item1 = yield self.conveyor.get()
+            item2 = yield self.conveyor.get()
+            yield env.timeout(3)  # 머신 B에서 제품을 완성하는 데 3시간 소요
+            print('시간 %d: 머신 B가 %s를 완성' % (env.now, item1))
+
 
 env = simpy.Environment()
-conveyor = simpy.Store(env, capacity=2)  # 컨베이어의 용량은 2
-env.process(machine_A(env, 'A', conveyor))
-env.process(machine_A(env, 'B', conveyor))
-env.process(machine_B(env, conveyor))
+conveyor1 = simpy.Store(env, capacity=1)
+conveyor2 = simpy.Store(env, capacity=1)
+# conveyor3 = simpy.Store(env, capacity=1)
 
-env.run(until=15)  # 15시간 동안 시뮬레이션 실행
+Product(env,'head' + str(1), conveyor1)
+Product(env,'body' + str(2), conveyor2)
+    # Product(env,'tail' + str(i+1), conveyor3)
+
+env.run(until=150)  # 15시간 동안 시뮬레이션 실행
