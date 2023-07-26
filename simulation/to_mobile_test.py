24f0bc970ca0dd3a25ef502da1ac312c2ef31708
HyeongChank <jsk381988@gmail.com>
2023-07-26 15:22:55 +0900

    update

---
+++ simulation/to_mobile_test.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from flask_socketio import SocketIO
+from flask_cors import CORS
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
+@socketio.on('message', namespace='/test')
+def handle_message(message):
+    print('received message: ' + message)
+    socketio.emit('message', message, namespace='/test')
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
