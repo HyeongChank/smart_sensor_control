7e85246002901626433085c554679a6d47cae9b6
HyeongChank <jsk381988@gmail.com>
2023-07-27 00:51:49 +0900

    update

--- simulation/to_mobile_test.py
+++ simulation/to_mobile_test.py
@@ -1,14 +1,16 @@
 from flask import Flask, request
 from flask_socketio import SocketIO
 from flask_cors import CORS
+
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-
 @socketio.on('message', namespace='/test')
 def handle_message(message):
     print('received message: ' + message)
+    if message == "connect":
+        print("connect success")
     socketio.emit('message', message, namespace='/test')
 
 if __name__ == '__main__':
