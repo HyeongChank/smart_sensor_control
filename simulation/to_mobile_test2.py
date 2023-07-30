8f7bdf7c8c8b07b08106e11222e049ffdfc401cc
HyeongChank <jsk381988@gmail.com>
2023-07-31 08:34:42 +0900

    update

--- simulation/to_mobile_test2.py
+++ simulation/to_mobile_test2.py
@@ -8,7 +8,7 @@ server_sock.bind((host, port))
 server_sock.listen(1)
  
 while True:
-    print("기다리는 중")
+    print("connecting....")
     client_sock, addr = server_sock.accept()
  
     print('Connected by', addr)
@@ -17,7 +17,7 @@ while True:
     print(data)
  
     while True:
-        # 클라이언트에서 받을 문자열의 길이
+        # 클라이언트에서 받을 문자열 길이
         data = client_sock.recv(4)
         length = int.from_bytes(data, "little")
         # 클라이언트에서 문자열 받기
